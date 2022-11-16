a2cdced0a4d457779eead96d0b234ff8f87f32d8
AM0312 <ansh.midha0312@gmail.com>
2022-11-17 02:24:58 +0530

    Updated counting sort

--- counting.cpp
+++ counting.cpp
@@ -9,16 +9,25 @@ void countingSort(int arr[], int n)
     int freq[largest + 1] = {0};
     for (int i = 0; i < n; i++)
         freq[arr[i]]++;
-    int j = 0;
-    for (int i = 0; i <= largest; i++)
-    {
-        while (freq[i] > 0)
-        {
-            arr[j] = i;
-            freq[i]--;
-            j++;
-        }
+    // int j = 0;
+    // for (int i = 0; i <= largest; i++)
+    // {
+    //     while (freq[i] > 0)
+    //     {
+    //         arr[j] = i;
+    //         freq[i]--;
+    //         j++;
+    //     }
+    // }
+    for(int i=1;i<=largest;i++)
+        freq[i]+=freq[i-1];
+    int temp[100];
+    for(int i=n-1;i>=0;i--){
+        temp[freq[arr[i]]]=arr[i];
+        freq[arr[i]]-=1;
     }
+    for(int i=0;i<n;i++)
+        arr[i]=temp[i+1];
 }
 
 int main()
